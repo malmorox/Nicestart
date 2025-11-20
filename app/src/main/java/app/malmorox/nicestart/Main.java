bd67bc806d67f57f067e88bf6aab688aa05aafe5
malmorox <marcos.almorox@gmail.com>
2025-11-20 09:52:10 +0100

    añadiendo context menu, appbar y swipe refresh en el main

--- app/src/main/java/app/malmorox/nicestart/Main.java
+++ app/src/main/java/app/malmorox/nicestart/Main.java
@@ -1,24 +1,93 @@
 package app.malmorox.nicestart;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView micontexto = findViewById(R.id.holaMundo);
+        registerForContextMenu(micontexto);
+
+        swipeLayout = findViewById(R.id.swipeRefresh);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+    }
+
+    // SWIPE REFRESH
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast = Toast.makeText(Main.this, "Ejemplo de Toast", Toast.LENGTH_LONG);
+            toast.show();
+
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
+    // MENU APPBAR
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.buscar) {
+            Toast toast = Toast.makeText(this, "Pulsado el buscar", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        if (id == R.id.favoritos) {
+            Toast toast = Toast.makeText(this, "Pulsado el favoritos", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // MENU CONTEXTUAL
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item1 seleccionado",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this, "Item2 seleccionado",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
     }
 }
